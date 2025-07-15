37bec48de62d90afd9170f454a167f0fbfaa6293
Vanaparthivarun <vanaparthivarun02@gmail.com>
2025-07-15 11:47:48 +0530

    Add files via upload

---
+++ task52.py
@@ -0,0 +1,8 @@
+original=[1,2,3,4,5,6,7,8,9,10]
+extracted=original[0:5]
+reverse=extracted[::-1]
+print("Original list:",original)
+print("Extracted first five elements:",extracted)
+print("Reversed extracted elements:",reverse)
+
+
